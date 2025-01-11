8d868729f47cf8fb5b8b724cea8feb6a42a21869
Othman Akhmouch <oakhmouc@c3r10p6.1337.ma>
2025-01-11 15:26:56 +0100

    backup

--- fract_ol/fract_ol.c
+++ fract_ol/fract_ol.c
@@ -1,11 +1,48 @@
 #include "fract_ol.h"
 
+void	draw_in_image(t_data image, int x, int y, int color)
+{
+	char	*cord;
+
+	image.addr = mlx_get_data_addr(&image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
+	cord = image.addr + (y * image.line_length + x * (image.bits_per_pixel / 8));
+	*(unsigned int *)cord = color;
+}
+
+void	draw_a_circle(t_data image, int x, int y)
+{
+	int	radius;
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	radius = 100;
+	while (a <= WIN_X)
+	{
+		b = 0;
+		while (b <= WIN_Y)
+		{
+			if ((a - x) * (a - x) + (b - y) * (b - y) <= radius * radius)
+			{
+				draw_in_image(image, a, b, COLOR);
+				b++;
+			}
+		}
+		a++;
+	}
+}
+
 int	main()
 {
 	w_data	window;
+	t_data	image;
 
 	window.mlx = mlx_init();
-	window.win = mlx_new_window(window.mlx, 1920, 1080, "Fract-ol");
+	window.win = mlx_new_window(window.mlx, WIN_X, WIN_Y, "Fract-ol");
+	image.img = mlx_new_image(window.mlx, WIN_X, WIN_Y);
+	draw_a_circle(image, WIN_X, WIN_Y);
+	mlx_put_image_to_window(window.mlx, window.win, image.img, 0, 0);
 	mlx_key_hook (window.win, esc_close, window.mlx);
 	mlx_hook (window.win, 17, 0, close_x, window.mlx);
 	mlx_loop (window.mlx);
--- fract_ol/fract_ol.h
+++ fract_ol/fract_ol.h
@@ -4,6 +4,9 @@
 # define SCROLL_DOWN 5
 # define SCROLL_UP 4
 # define LEFT_CLICK 1
+# define WIN_X 1920 / 2
+# define WIN_Y 1080 / 2
+# define COLOR 0x00FF0000
 # include </home/oakhmouc/minilibx-linux/mlx.h>
 # include <math.h>
 # include <stdlib.h>
@@ -16,6 +19,14 @@ typedef struct	w_data
 	void	*mlx;
 	void	*win;
 }				w_data;
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 int	esc_close(int the_code, w_data window);
 int close_x(w_data *window);
